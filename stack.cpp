a10d7c6eb0089e071afd5fe493ddc8d8d1318b69
Stanislav Kurnevich <70342945+StanislavKy@users.noreply.github.com>
2021-01-29 20:17:41 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -14,18 +14,21 @@ typedef double elem_t;
 typedef struct Stack
 {
     size_t  capacity; // макс колво в стеке
-    size_t  size; // колво элементов 
+    size_t  size;     // колво элементов 
     elem_t* data;
+    size_t  hash;
 }  Stack_t;
 
 //-----------------------------------------------------------------------------------------
 
-void   stack_destruct     (Stack_t* stack); 
-void   stack_realloc      (Stack_t* stack); 
-void   push (Stack_t* stack, elem_t value);
-
 Stack_t* stack_construct  (Stack_t* stack);
 elem_t   stack_pop        (Stack_t* stack);
+
+void     stack_verificate (Stack_t* stack);
+void     stack_destruct   (Stack_t* stack); 
+void     stack_realloc    (Stack_t* stack); 
+
+void  stack_push (Stack_t* stack, elem_t value);
 
 //-----------------------------------------------------------------------------------------
 
@@ -34,6 +37,24 @@ int main()
     Stack_t stk = {};
 
     stack_construct (&stk);
+    
+    stack_push (&stk, 10);
+    stack_push (&stk, 11);
+    stack_push (&stk, 12);
+    stack_push (&stk, 13);
+    stack_push (&stk, 14);
+    stack_push (&stk, 15);
+    stack_push (&stk, 16);
+    stack_push (&stk, 17);
+    stack_push (&stk, 18);
+    stack_push (&stk, 19);
+    stack_push (&stk, 20);
+    stack_push (&stk, 21);
+    printf ("%f", stack_pop  (&stk));
+    
+
+    for (int i = 0; i < stk.capacity; i++) printf ("%f\n", *(stk.data + i));
+
     stack_destruct  (&stk); 
 }
 
@@ -48,6 +69,8 @@ Stack_t* stack_construct (Stack_t* stack)
     
     assert ((stack -> data) != NULL);
     
+    for (int i = 0; i < stack -> capacity; i++) *(stack -> data + i) = POISON_XXX;
+
     stack -> size = 0;
     return stack;
 }
@@ -67,20 +90,30 @@ void stack_destruct (Stack_t* stack)
 
 void stack_realloc (Stack_t* stack) 
 {
+    stack_verificate (stack);
+    
+    size_t old_capacity = stack -> capacity;
+
     stack -> capacity *= X_CAPACITY;
     stack -> data      = (elem_t*) realloc (stack -> data, (stack -> capacity) * sizeof (elem_t));
     
-    assert ((stack -> data) == NULL);
+    for (int i = old_capacity; i < stack -> capacity; i++) *(stack -> data + i) = POISON_XXX;
+
+    assert ((stack -> data) != NULL);
 }
 
 //-----------------------------------------------------------------------------------------
 
 void stack_push (Stack_t* stack, elem_t value) 
 {
-    if ((stack -> size) >= (stack -> capacity)) 
+    assert (stack != NULL);
+    assert ((stack -> data) != 0);
+    
+    if ((stack -> size + 1) >= (stack -> capacity)) 
     {
         stack_realloc (stack);
     }
+
     stack -> data [stack -> size] = value;
     stack -> size++;
 }
@@ -89,10 +122,21 @@ void stack_push (Stack_t* stack, elem_t value)
 
 elem_t stack_pop (Stack_t* stack) 
 {
-    assert ((stack -> size) == 0);
+    stack_verificate (stack);
+
+    elem_t out = *(stack -> data + stack -> size);
+    *(stack -> data + stack -> size) = POISON_XXX; 
+
     stack -> size--;
 
-    return stack -> data[stack -> size];
+    return out;
 }
 
 //-----------------------------------------------------------------------------------------
+
+void stack_verificate (Stack_t* stack)
+{
+    assert (stack != NULL);
+    assert ((stack -> size) != 0);
+    assert ((stack -> data) != 0);
+}
